edc919b742adeda62ce6919369880ca8b3ea13ef
Pedro Henrique Alves Ferreira <pedrohenriquealves5191@gmail.com>
2026-06-07 11:59:13 -0300

    Add pasta com assets

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from src.config import(
     METEORO_LARGURA ,
     METEORO_VEL,
     PRETO,
-    FONTE
+    FONTE,
+    JOGADOR_NAVE_IMAGEM
 )
 from src.funcoes import (
     jogador_movimentacao,
@@ -25,25 +26,14 @@ pygame.font.init()
 
 #utilizando as variáveos de config
 TELA = pygame.display.set_mode((TELA_LARGURA, TELA_ALTURA))
-pygame.display.set_caption("Nave Py")
+pygame.display.set_caption(TITULO_JOGO)
 
-def gerar_meteoro(meteoros):
-    while True:
-        meteoro_x = random.randint(0, TELA_LARGURA - METEORO_LARGURA)
-        meteoro_y = random.randint(-600, -METEORO_ALTURA)
-        novo = pygame.Rect(meteoro_x, meteoro_y, METEORO_LARGURA, METEORO_ALTURA)
-        colisao = False
-
-        for m in meteoros:
-            if novo.colliderect(m):
-                colisao = True
-                break
-        
-        if not colisao:
-            return novo
+JOGADOR_NAVE = pygame.transform.scale(JOGADOR_NAVE_IMAGEM, (JOGADOR_LARGURA,JOGADOR_ALTURA))
 
 def desenhar(jogador, tempo_corrido, vidas, meteoros):
-    pygame.draw.rect(TELA, (255,255,255), jogador)
+    TELA.fill(PRETO)
+
+    TELA.blit(JOGADOR_NAVE, (jogador.x, jogador.y))
 
     tempo_string = FONTE.render(f"{round(tempo_corrido)}s", 1, "white")
     TELA.blit(tempo_string, (tempo_string.get_width(), TELA_ALTURA - tempo_string.get_height() - 10))
@@ -62,14 +52,13 @@ def main():
     pygame.init()
     run = True
 
-    #contador de tempo de jogo
     tempo_inicio = time.time()
     tempo_corrido = 0
 
     clock = pygame.time.Clock()
     jogador = pygame.Rect((TELA_LARGURA - JOGADOR_LARGURA) // 2, TELA_ALTURA - JOGADOR_ALTURA * 2, JOGADOR_LARGURA, JOGADOR_ALTURA)
     vidas = JOGADOR_VIDAS
-    #tempo para adicionar novos meteoros e lista para guarda-los
+    
     meteoro_add = 2000
     meteoro_contador = 0
     meteoros = []
@@ -100,14 +89,13 @@ def main():
             meteoro.y += METEORO_VEL
             if meteoro.y > TELA_ALTURA:
                 meteoros.remove(meteoro)
-            elif meteoro.y + meteoro.height >= jogador.y and meteoro.colliderect(jogador):
+            elif meteoro.colliderect(jogador):
                 meteoros.remove(meteoro)
                 hit = True
                 break
         if hit:
             vidas -= 1
 
-        TELA.fill(PRETO)
         desenhar(jogador, tempo_corrido, vidas, meteoros)
 
     pygame.quit()
--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 # Configurações centrais do jogo (tela, cores e caminhos de arquivos).
 import pygame
+import os
 pygame.font.init()
 
 TELA_LARGURA = 1280
@@ -8,10 +9,11 @@ FPS = 60
 
 TITULO_JOGO = "Projeto Final - Pygame"
 
-JOGADOR_ALTURA = 60
-JOGADOR_LARGURA = 40
+JOGADOR_ALTURA = 100
+JOGADOR_LARGURA = 100
 JOGADOR_VEL = 6
 JOGADOR_VIDAS = 3
+JOGADOR_NAVE_IMAGEM = pygame.image.load("assets\imagens\jogador_nave.png")
 
 METEORO_ALTURA = 50
 METEORO_LARGURA = 50
